5a8446e7f0639e6415908ce4fd43eb85890e0443
Rob Smith <sleepingkingstudios@gmail.com>
2012-01-12 13:16:20 -0500

    Initial commit; created plugin TextExplorer, set up lib and spec directories

---
+++ lib/text_explorer.rb
@@ -0,0 +1,15 @@
+# lib/text_explorer.rb
+
+require 'round_table'
+require 'vendor/plugins'
+
+module RoundTable::Vendor::Plugins
+  # The TextExplorer plugin adds functionality for exploring virtual spaces
+  # through parsed-text input and short prose output, in the style of classic
+  # interactive fiction or adventure games.
+  module TextExplorer
+    
+  end # module TextExplorer
+end # module RoundTable::Vendor::Plugins
+
+include RoundTable::Vendor::Plugins
---
+++ spec/spec_helper.rb
@@ -0,0 +1,71 @@
+# spec/spec_helper.rb
+
+current_path = File.expand_path(File.dirname __FILE__)
+$LOAD_PATH << current_path.gsub(/vendor[\w\/]+/, "lib")
+
+###############################################################################
+# SET UP LOGGING
+###############################################################################
+
+require 'debug/file_logger'
+require 'debug/logger_service'
+
+include RoundTable::Debug::LoggerService
+
+file_path = "#{Dir.pwd}/log/log_spec.txt"
+File.new(file_path, "w+") unless File.exists?(file_path)
+File.open(file_path, "w+") { |file| file.truncate(0) }
+
+logger = RoundTable::Debug::FileLogger.new file_path
+RoundTable::Debug::LoggerService::StoredLogger.logger = logger
+
+logger.format = "\n\n%m"
+logger.info "Running spec_helper..."
+logger.format = "%L %m"
+
+###############################################################################
+# CUSTOM MATCHERS
+###############################################################################
+
+RSpec::Matchers.define :include_matching do |expected|
+  match do |actual|
+    actual.inject(false) { |memo, obj| memo || obj =~ expected }
+  end # match do |actual|
+  
+  failure_message_for_should do |actual|
+    "expected that #{actual} would include an element matching #{expected}"
+  end # failure_message_for_should
+  
+  failure_message_for_should_not do |actual|
+    "expected that #{actual} would not include an element matching #{expected}"
+  end # failure_message_for_should_not
+end # define :include_matching
+
+###############################################################################
+# SPEC HELPERS
+###############################################################################
+
+require 'events/event_dispatcher'
+
+module RSpec
+  module Helpers
+    def self.capture_events(subject, event_type = :*)
+      ary = Array.new
+      subject.add_listener event_type, Proc.new { |evt|
+        ary << evt
+      } # end listener :*
+      yield
+      ary
+    end # method self.capture_events
+  end # module Helpers
+end # module RSpec
+
+###############################################################################
+# SET UP SPECS
+###############################################################################
+
+require 'text_explorer'
+
+module TextExplorer
+  module Mock; end
+end # module TextExplorer
